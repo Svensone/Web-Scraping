396a2f5ec58c375bb6e0554df5dda3f274c06ac5
svenson.e <svenson.ertel@gmail.com>
2020-11-22 11:24:51 +0800

    pipeline.py still bugging

--- [Book}WebScrapyingO'Reilly/Ch.5 Scrapy/wikiSpider/wikiSpider/pipelines.py
+++ [Book}WebScrapyingO'Reilly/Ch.5 Scrapy/wikiSpider/wikiSpider/pipelines.py
@@ -14,21 +14,18 @@ from string import whitespace
 ## don't forget to add Pipeline in settings.py
 class WikispiderPipeline(object):
     # process_item mandatory (used to process data so parse_items in spider focus on requests)
+    def process_item(self, Article, spider):
 
-    def process_item(self, article, spider):
-
-        # process 'lastUpdatet' - delete in spider parse_items function
-        # dateStr = article['lastUpdated']
-        # previously done in parse_items()
-        article['lastUpdated'] = article['lastUpdated'].replace("This page was last edited on", '')
-        # convert str to python 'datetime' format (strip whitespace before)
-
+        ## process 'lastUpdatet' - delete in spider parse_items function
+        ## previously done in parse_items()
+        Article['lastUpdated'] = Article['lastUpdated'].replace('This page was last edited on', '')
+        ### convert str to python 'datetime' format (strip whitespace before)
         ### comment out next lines since error in processing lastUpdated
-        # article['lastUpdated'] = article['lastUpdated'].strip()
-        # article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H:%M.')
+        Article['lastUpdated'] = Article['lastUpdated'].strip()
+        Article['lastUpdated'] = datetime.strptime(Article['lastUpdated'], '%d %B %Y, at %H:%M.')
         
-        # clean article['text']
-        article['text'] = [line for line in article['text'] if line not in whitespace]
+        # clean Article['text']
+        Article['text'] = [line for line in Article['text'] if line not in whitespace]
+        Article['text'] = ''.join(Article['text'])
         # .join() can be used also on Dict ('key') thus versatiler than concatenate
-        article['text'] = ''.join(article['text'])
-        return article
+        return Article
