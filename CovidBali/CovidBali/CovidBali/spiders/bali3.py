efa83fc56f501723698d1460516803ab11aef8ec
svenson.e <svenson.ertel@gmail.com>
2020-11-25 15:56:33 +0800

    scrapying diff. dates

--- CovidBali/CovidBali/CovidBali/spiders/bali3.py
+++ CovidBali/CovidBali/CovidBali/spiders/bali3.py
@@ -10,7 +10,9 @@ from datetime import timedelta, date
 class BaliCrawl(CrawlSpider):
     name = 'bali3'
 
-    allowed_domains = []
+    allowed_domains = [
+        "pendataan.baliprov.go.id"
+    ]
     
     start_date = date(2020, 11, 1)
     end_date = date(2020, 11, 10) 
@@ -19,7 +21,6 @@ class BaliCrawl(CrawlSpider):
         # get the dates wanted
         for n in range((end_date - start_date).days):
             yield start_date + timedelta(n)
-    
 
     start_urls = []
     start_url = "https://pendataan.baliprov.go.id/map_covid19/search?_token=cxTcYY048Tq8V2BvucUMNEBqvZxZmmMWGPfOqhJ4&level=kabupaten&kabupaten=&tanggal="
@@ -28,17 +29,21 @@ class BaliCrawl(CrawlSpider):
     for single_date in daterange(start_date, end_date):
         start_urls.append(single_date.strftime(start_url+ "%Y-%m-%d"))
     
-    Rule(LinkExtractor(), callback='parse_item', follow=True)
-
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//td[@class='example__example']/a"), callback='parse_item', follow=True),)
     
     def parse_item(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'baliCovid-{page}.html'
 
-        url = response.url
-        date = response.url[-9:]
+        # step 3:
+        data = []
+        for row in response.css('tbody tr'):
+            data.append(row.css('td::text').getall())
+            
+        update_date = response.css('div.alert::text')[1].get()
 
-        print(date)
-        
         yield {
-            'url': url,
-            'date': date
-        }
+            'data': data,
+            'update_date': update_date,
+            }
