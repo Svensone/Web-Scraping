9ab7172034f87a1928009f88ec7d490600aef9d2
svenson.e <svenson.ertel@gmail.com>
2020-12-05 06:44:02 +0800

    cleaning df and geojson for specific baliregencies

---
+++ CovidBali/CovidBali/CovidBali/BaliDashApp/baliDashApp.py
@@ -0,0 +1,13 @@
+
+## Bali Covid-Cases per Regency Dash Web App
+
+import pandas as pd
+import numpy as np
+import json
+
+import plotly.express as px
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+from dash_dependencies import Input, Output, State
