f8397d53bc49edfdb9d083f059e08f72484d63c6
svenson.e <svenson.ertel@gmail.com>
2020-12-11 07:35:39 +0800

    adding daily numbers

--- CovidBali/BaliDashApp/baliDashApp.py
+++ CovidBali/BaliDashApp/baliDashApp.py
@@ -2,7 +2,6 @@
 ## Bali Covid-Cases per Regency Dash Web App
 
 import pandas as pd
-import numpy as np
 import json
 
 import plotly.express as px
@@ -61,74 +60,11 @@ controls = dbc.FormGroup(
             options=[{
                 'label': 'Value One',
                 'value': 'value1'
-            }, {
-                'label': 'Value Two',
-                'value': 'value2'
-            },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
+            }
             ],
             value=['value1'],  # default value
             multi=True
         ),
-        html.Br(),
-        html.P('Range Slider', style={
-            'textAlign': 'center'
-        }),
-        dcc.RangeSlider(
-            id='range_slider',
-            min=0,
-            max=20,
-            step=0.5,
-            value=[5, 15]
-        ),
-        html.P('Check Box', style={
-            'textAlign': 'center'
-        }),
-        dbc.Card([dbc.Checklist(
-            id='check_list',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            },
-                {
-                    'label': 'Value Two',
-                    'value': 'value2'
-                },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value=['value1', 'value2'],
-            inline=True
-        )]),
-        html.Br(),
-        html.P('Radio Items', style={
-            'textAlign': 'center'
-        }),
-        dbc.Card([dbc.RadioItems(
-            id='radio_items',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            },
-                {
-                    'label': 'Value Two',
-                    'value': 'value2'
-                },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value='value1',
-            style={
-                'margin': 'auto'
-            }
-        )]),
         html.Br(),
         dbc.Button(
             id='submit_button',
@@ -181,33 +117,6 @@ content_first_row = dbc.Row([
 
         ),
         md=3
-    ),
-    dbc.Col(
-        dbc.Card(
-            [
-                dbc.CardBody(
-                    [
-                        html.H4('Card Title 3', className='card-title', style=CARD_TEXT_STYLE),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
-                    ]
-                ),
-            ]
-
-        ),
-        md=3
-    ),
-    dbc.Col(
-        dbc.Card(
-            [
-                dbc.CardBody(
-                    [
-                        html.H4('Card Title 4', className='card-title', style=CARD_TEXT_STYLE),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
-                    ]
-                ),
-            ]
-        ),
-        md=3
     )
 ])
 
@@ -215,12 +124,6 @@ content_second_row = dbc.Row(
     [
         dbc.Col(
             dcc.Graph(id='graph_1'), md=4
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_2'), md=4
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_3'), md=4
         )
     ]
 )
@@ -233,16 +136,6 @@ content_third_row = dbc.Row(
     ]
 )
 
-content_fourth_row = dbc.Row(
-    [
-        dbc.Col(
-            dcc.Graph(id='graph_5'), md=6
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_6'), md=6
-        )
-    ]
-)
 # Content Component
 content = html.Div(
     [
@@ -251,7 +144,6 @@ content = html.Div(
         content_first_row,
         content_second_row,
         content_third_row,
-        # content_fourth_row
     ],
     style=CONTENT_STYLE
 )
@@ -270,15 +162,11 @@ app.layout = html.Div([sidebar, content])
 @app.callback(
     Output('graph_1', 'figure'),
     [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
+    [State('dropdown', 'value')
      ])
-def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+def update_graph_1(n_clicks, dropdown_value):
     print(n_clicks)
     print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
 
     # Bar Chart new and total cases per regency
     path_data = r'C:\Users\ansve\Coding\Projects-WebScraping\CovidBali\BaliDashApp\datasets'
@@ -290,58 +178,15 @@ def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_valu
     fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
     return fig
 
-
-@app.callback(
-    Output('graph_2', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_2(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    fig = {
-        'data': [{
-            'x': [1, 2, 3],
-            'y': [3, 4, 5],
-            'type': 'bar'
-        }]
-    }
-    return fig
-
-
-@app.callback(
-    Output('graph_3', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_3(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    df = px.data.iris()
-    fig = px.density_contour(df, x='sepal_width', y='sepal_length')
-    return fig
-
-
 @app.callback(
     Output('graph_4', 'figure'),
     [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
+    [State('dropdown', 'value')
      ])
 def update_graph_4(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
     print(n_clicks)
     print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  
+     
     ## Bali Regency Covid Cases 
 
     # Sample data and figure
@@ -363,41 +208,6 @@ def update_graph_4(n_clicks, dropdown_value, range_slider_value, check_list_valu
         'height': 600
     })
     return fig
-
-
-# @app.callback(
-#     Output('graph_5', 'figure'),
-#     [Input('submit_button', 'n_clicks')],
-#     [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-#      State('radio_items', 'value')
-#      ])
-# def update_graph_5(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-#     print(n_clicks)
-#     print(dropdown_value)
-#     print(range_slider_value)
-#     print(check_list_value)
-#     print(radio_items_value)  # Sample data and figure
-#     df = px.data.iris()
-#     fig = px.scatter(df, x='sepal_width', y='sepal_length')
-#     return fig
-
-
-# @app.callback(
-#     Output('graph_6', 'figure'),
-#     [Input('submit_button', 'n_clicks')],
-#     [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-#      State('radio_items', 'value')
-#      ])
-# def update_graph_6(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-#     print(n_clicks)
-#     print(dropdown_value)
-#     print(range_slider_value)
-#     print(check_list_value)
-#     print(radio_items_value)  # Sample data and figure
-#     df = px.data.tips()
-#     fig = px.bar(df, x='total_bill', y='day', orientation='h')
-#     return fig
-
 
 @app.callback(
     Output('card_title_1', 'children'),
